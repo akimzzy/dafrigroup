abe2a04a16074d74b7d9ea68291f1e180edf1e86
akimzzy <a.akeem@yahoo.com>
2020-10-03 08:38:37 +0100

    removed unused

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 
 import { hydrate, render } from "react-dom";
 
-const APP = (App)
  
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
